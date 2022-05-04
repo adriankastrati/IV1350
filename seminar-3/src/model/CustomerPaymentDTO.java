bef4a53bee7d0d26d2f0a0b8083c1abb9644d858
Adrian Kastrati <77054963+adriankastrati@users.noreply.github.com>
2022-05-04 16:15:32 +0200

    working without testers

---
+++ seminar-3/src/model/CustomerPaymentDTO.java
@@ -0,0 +1,11 @@
+package model;
+
+public class CustomerPaymentDTO {
+    double amountToPay;
+    double change;
+
+    public CustomerPaymentDTO(CustomerPayment customerPaymentToDTO){
+
+
+    }
+}
