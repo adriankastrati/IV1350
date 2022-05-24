be47994d9d0781e70bae10555e87f9df471874c4
Adrian Kastrati <77054963+adriankastrati@users.noreply.github.com>
2022-05-24 13:25:07 +0200

    tests fot server down and item not found completed

--- seminar-4/src/se.kth.processSale.integration/DeveloperLog.java
+++ seminar-4/src/se.kth.processSale.integration/DeveloperLog.java
@@ -4,8 +4,16 @@ package se.kth.processSale.integration;
  * Represents the developer log that is written to when exceptions are thrown
  */
 public class DeveloperLog {
+
+    /**
+     * creates a new instance of the developer log
+     */
     public DeveloperLog(){}
 
+    /**
+     * Writes the Exception to the "log"( in our case the console)
+     * @param exceptionMessage - message from exception
+     */
     public void addExceptionToLog(String exceptionMessage){
         System.out.println("Developer Log:"+ exceptionMessage + "\n");
     }
